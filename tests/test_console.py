c9c36c2be9dc2c3a00df937f4edbedf881f8ffde
Jason Cortella <419@holbertonschool.com>
2019-08-15 14:20:17 +0000

    First edits to unittests

--- tests/test_console.py
+++ tests/test_console.py
@@ -17,6 +17,10 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from models import storage
+import re
+import MySQLdb
 
 
 class TestConsole(unittest.TestCase):
