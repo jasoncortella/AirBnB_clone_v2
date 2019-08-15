ff4b08a67f0c326f7ea111fbdbfe7e78eacc6274
Jason Cortella <419@holbertonschool.com>
2019-08-15 23:22:32 +0000

    pep8

--- models/amenity.py
+++ models/amenity.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
 
+
 class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
