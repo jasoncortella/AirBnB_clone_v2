fdd6f6b1573fb752badb739f1c3dc95a5ffb21b3
Jason Cortella <419@holbertonschool.com>
2019-09-03 21:34:33 +0000

    Iniital working commit p7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,69 +4,26 @@ Starts a Flask web application with the following conditions
 
 - listens on 0.0.0.0, port 5000
 - routes
-  - /:                       display “Hello HBNB!”
-  - /hbnb:                   display “HBNB”
-  - /c/<text>:               display 'C ' followed by <text>
-                               - replaces underscores _ with spaces
-  - /python/(<text>):        display 'Python ', followed by <text>
-                               - replaces underscores _ with spaces
-  - /number/<n>:             display "n is a number" only if n is an integer
-  - /number_template/<n>:    display a HTML page only if n is an integer
-                               - H1 tag: “Number: n” inside the tag BODY
-  - /number_odd_or_even/<n>: display a HTML page only if n is an integer
-                               - H1 tag: “Number: n is even|odd”
-                               - inside the tag BODY
+  - /states_list: lists state objects in dbstorage
 """
 
 from flask import Flask
 from flask import render_template
+from models import storage
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_route():
-    """ Displays 'Hello HBNB!' """
-    return 'Hello HBNB!'
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ displays state objects in dbstorage """
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
 
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb_route():
-    """ Displays 'HBNB' """
-    return 'HBNB'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """ Displays 'C ' followed by the value of the `text` variable """
-    text = text.replace("_", " ")
-    return 'C ' + text
-
-
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_route(text="is cool"):
-    """ Displays 'Python ' followed by the value of the `text` variable """
-    text = text.replace("_", " ")
-    return 'Python ' + text
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
-    """ Displays 'n is a number' only if n is an integer """
-    return str(n) + ' is a number'
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """ display a HTML page only if n is an integer """
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    """ display a HTML page only if n is an integer """
-    return render_template('6-number_odd_or_even.html', n=n)
+@app.teardown_appcontext
+def teardown(self):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
