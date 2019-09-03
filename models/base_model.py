684962cfcd522b4268a5625b78f6a48346d238e6
Jason Cortella <419@holbertonschool.com>
2019-09-03 19:22:37 +0000

    Remove incorrect deletion of sa_instance_state

--- models/base_model.py
+++ models/base_model.py
@@ -42,13 +42,13 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-            if self.id is None:
+            if "id" not in kwargs:
                 self.id = str(uuid.uuid4())
             datetimeNow = datetime.now()
             # Use variable assignment to guarantee matching created/updated at
-            if self.created_at is None:
+            if "created_at" not in kwargs:
                 self.created_at = datetimeNow
-            if self.updated_at is None:
+            if "updated_at" not in kwargs:
                 self.updated_at = datetimeNow
         else:
             self.id = str(uuid.uuid4())
@@ -59,9 +59,8 @@ class BaseModel:
         Return:
             returns a string of class name, id, and dictionary
         """
-        self.__dict__.pop('_sa_instance_state', None)
         return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
+            type(self).__name__, self.id, self.to_dict())
 
     def __repr__(self):
         """return a string representaion
@@ -71,7 +70,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at to current
         """
-        # self.updated_at = datetime.now()
+        self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
 
